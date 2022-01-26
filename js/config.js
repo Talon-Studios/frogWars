edb3251f5ec3f3e8f35462c0c8c492a5f94dc230
thcheetah777 <thcheetah777@gmail.com>
2022-01-25 17:37:07 -0800

    Add some files

---
+++ js/config.js
@@ -0,0 +1,26 @@
+const config = {
+  type: Phaser.AUTO,
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.RESIZE
+  },
+  render: {
+    pixelArt: true
+  },
+  backgroundColor: 0x000000,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 1500
+      },
+      enableBody: true,
+      // debug: true
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+};
