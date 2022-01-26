f2ed4f4075e6c1ffa61277ccebfbf69055383a6c
thcheetah777 <thcheetah777@gmail.com>
2022-01-25 17:47:55 -0800

    Load images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function preload() {
-
+  this.load.image("tile0", "assets/tile0.png");
+  this.load.image("tile1", "assets/tile1.png");
+  this.load.image("tile0select", "assets/tile0select.png");
+  this.load.image("tile1select", "assets/tile1select.png");
 }
 function create() {
 
