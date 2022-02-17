afb6d316d0bc40cc68a5b3e0a51c5d1d57c0f72b
thcheetah777 <thcheetah777@gmail.com>
2022-02-17 15:27:38 -0800

    Change overlap into collider

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ class Game extends Phaser.Scene {
     });
 
     // ---------- Colliders ----------
-    this.physics.add.overlap(game.frogs, game.robots, (frog, robot) => {
+    this.physics.add.collider(game.frogs, game.robots, (frog, robot) => {
       if (frog.type === "basic") {
         frog.destroy();
         let lastFrame = robot.texture.key;
@@ -166,7 +166,7 @@ class Game extends Phaser.Scene {
           }, 300);
         }
       }
-    });
+    }
     this.physics.add.collider(game.projectiles, game.robots, (projectile, robot) => {
       projectile.destroy();
       let lastFrame = robot.texture.key;
