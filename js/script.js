44de30cceb3e31f9278343ebd15f965c70622882
thcheetah777 <thcheetah777@gmail.com>
2022-02-27 14:51:50 -0800

    Auto basic frog

--- js/script.js
+++ js/script.js
@@ -321,8 +321,8 @@ class Game extends Phaser.Scene {
       delay: 50,
       callback: () => {
         let row = Math.floor(Math.random() * game.height);
-        let frog = game.frogs.create(tile.x, (game.TILESIZE / 2 + game.TILESIZE * row) + game.topMargin, `basicFrog0`).setScale(8).setGravityY(-1500).setSize(7, 8).setOffset(0, 0).setImmovable();
-        frog.type = game.currentSelection;
+        let frog = game.frogs.create(0, (game.TILESIZE / 2 + game.TILESIZE * row) + game.topMargin, `basicFrog0`).setScale(8).setGravityY(-1500).setSize(7, 8).setOffset(0, 0).setImmovable();
+        frog.type = "basic";
       },
       callbackScope: this,
       repeat: -1
