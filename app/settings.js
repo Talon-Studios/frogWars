1383993b88afd2563fc385359237c6ada312a230
thcheetah777 <thcheetah777@gmail.com>
2022-03-20 15:28:59 -0700

    Move the scenes to seperate files

---
+++ app/settings.js
@@ -0,0 +1,85 @@
+// Frog Wars
+/*^*^*^*^*^*^*^*
+settings.js
+The settings page for sfx and music.
+*^*^*^*^*^*^*^*/
+
+class Settings extends Phaser.Scene {
+  constructor() {
+    super("Settings");
+    this.sfx = {};
+  }
+  preload() {
+    this.load.audio("optionSelect", "assets/optionSelect.wav");
+    this.load.audio("introMusic", "assets/introMusic.mp3");
+    this.load.image("cursor", "assets/cursor.png");
+    this.load.image("checkboxChecked", "assets/checkboxChecked.png");
+    this.load.image("checkboxUnchecked", "assets/checkboxUnchecked.png");
+    this.load.image("musicSetting", "assets/musicSetting.png");
+    this.load.image("sfxSetting", "assets/sfxSetting.png");
+    this.load.image("backSetting", "assets/backSetting.png");
+  }
+  create() {
+    this.engine = new Engine(this);
+
+    // Add sounds
+    this.sfx.optionSelect = this.sound.add("optionSelect");
+
+    // Create cursor
+    this.engine.mouseInput();
+    game.cursor = this.physics.add.sprite(this.input.mousePointer.x, this.input.mousePointer.y, "cursor").setScale(8).setGravityY(-1500).setSize(2, 2).setOffset(0, 0).setOrigin(0, 0);
+    game.cursor.setDepth(1);
+
+    // Set background color
+    this.engine.setBackgroundColor(this, "#ffffff");
+
+    // Create text
+    this.musicSetting = this.add.image(this.engine.gameWidthCenter + 42, 200, "musicSetting").setScale(8).setInteractive();
+    this.sfxSetting = this.add.image(this.engine.gameWidthCenter + 70, 300, "sfxSetting").setScale(8).setInteractive();
+
+    // Create checkboxes
+    this.musicCheckbox = this.add.image(this.engine.gameWidthCenter - 180, 190, "checkboxUnchecked").setScale(8).setInteractive();
+    if (game.musicEnabled) this.musicCheckbox.setTexture("checkboxChecked");
+    this.sfxCheckbox = this.add.image(this.engine.gameWidthCenter - 180, 290, "checkboxUnchecked").setScale(8).setInteractive();
+    if (game.sfxEnabled) this.sfxCheckbox.setTexture("checkboxChecked");
+
+    // Create backbutton
+    this.backSetting = this.add.image(this.engine.gameWidthCenter, 490, "backSetting").setScale(8).setInteractive();
+
+    // Interaction
+    this.musicCheckbox.on("pointerdown", () => {
+      game.musicEnabled = !game.musicEnabled;
+      if (game.musicEnabled) {
+        this.musicCheckbox.setTexture("checkboxChecked");
+        localStorage.setItem("musicEnabled", true);
+      } else {
+        this.musicCheckbox.setTexture("checkboxUnchecked");
+        localStorage.setItem("musicEnabled", false);
+      }
+    });
+    this.sfxCheckbox.on("pointerdown", () => {
+      game.sfxEnabled = !game.sfxEnabled;
+      if (game.sfxEnabled) {
+        this.sfxCheckbox.setTexture("checkboxChecked");
+        localStorage.setItem("sfxEnabled", true);
+      } else {
+        this.sfxCheckbox.setTexture("checkboxUnchecked");
+        localStorage.setItem("sfxEnabled", false);
+      }
+    });
+    this.backSetting.on("pointerup", () => {
+      this.scene.stop();
+      this.scene.start("Start");
+    });
+    this.input.on("pointerdown", () => {
+      game.cursor.setScale(6.5);
+    });
+    this.input.on("pointerup", () => {
+      game.cursor.setScale(8);
+    });
+  }
+  update() {
+    game.cursor.x = this.input.mousePointer.x;
+    game.cursor.y = this.input.mousePointer.y;
+  }
+}
