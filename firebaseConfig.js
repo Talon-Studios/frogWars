e42ba8b198feb2f8a2bd52ac5efbbebf4c707512
thcheetah777 <thcheetah777@gmail.com>
2022-04-03 11:25:51 -0700

    Add the config file for firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// Frog Wars
+/*^*^*^*^*^*^*^*
+firebaseConfig.js
+The config file for firebase.
+*^*^*^*^*^*^*^*/
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuDcgDrUA-2-QKiKYRF6QtRslm3uOjX58",
+  authDomain: "frog-wars.firebaseapp.com",
+  projectId: "frog-wars",
+  storageBucket: "frog-wars.appspot.com",
+  messagingSenderId: "1011270311468",
+  appId: "1:1011270311468:web:6ca79f863ca3c966e3fb89",
+  measurementId: "G-FZNYL990MK"
+};
